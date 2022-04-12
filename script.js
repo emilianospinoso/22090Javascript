df94080b9468881a635e5578c185d35bad0168a2
emilianospinoso <emilianospinoso@gmail.com>
2022-04-12 14:20:08 -0300

    Clase Javascript

---
+++ script.js
@@ -0,0 +1,52 @@
+//Aca meto un popup
+//alert("HolA MUNDO")
+/*
+serian 
+comentarios
+*//*
+let Alumno
+let _apellido
+let $edad
+let nombreApellido = "emiliano spinoso"
+let nombre_apellido = "ana maria"
+let NombreApellido= "ana clara"
+*/
+///Variables que estan mal
+/*
+let 1ernombre
+let nombre-appelido;
+let super , beak , this , 
+*/
+/*
+const entidad =  "Gobierno buenos aires";
+entidad ="Nacion"
+
+console.log("La entidad es :" + entidad);
+*/
+
+//////TIPOS DE DATOS/////////////
+
+//Strings
+let str1 =  "String con comillas dobles "
+let str2 = 'String con comillas simples'
+let str3 = `String con Backticks`
+
+let str5 = `${str1} y ademas ${str2} `
+let str6 = str1 + " y ademas " + str2;
+
+//Numeros
+let nombre = prompt("Como te llamas?")
+let  numero1 =  parseFloat(prompt("mete el numero 1"));//1.5
+let numero2 =   parseFloat(prompt("mete el segundo numero"));//1.9
+
+let resta =  numero1-numero2
+let suma = numero1+numero2 
+let multi = numero1*numero2
+let division = numero1/numero2
+
+alert("Hola" + nombre)
+alert( "La suma es :" + suma)
+alert( "La resta es :" + resta)
+alert( "La multiplicacion  es :" + multi)
+
+
